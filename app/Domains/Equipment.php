41e3d78aad35c7262d7fe0619272aee8add67988
fuminopen <fuminopen@gmail.com>
2022-06-11 21:08:16 +0900

    [add]equipment enhancement logic

---
+++ app/Domains/Equipment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Domains;
+
+final class Equipment
+{
+    /**
+     * @var int
+     */
+    private int $currentLevel;
+
+    /**
+     * __construct
+     *
+     * @param  int $currentLevel
+     * @return void
+     */
+    public function __construct(int $currentLevel = 0)
+    {
+        $this->currentLevel = $currentLevel;
+    }
+
+    /**
+     * getCurrentLevel
+     *
+     * @return int
+     */
+    public function getCurrentLevel(): int
+    {
+        return $this->currentLevel;
+    }
+
+    /**
+     * enhance
+     *
+     * @return int
+     */
+    public function enhance(): int
+    {
+        $this->currentLevel++;
+
+        return $this->currentLevel;
+    }
+}
