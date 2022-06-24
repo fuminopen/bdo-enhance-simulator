2a273d969a1ac7c2db8ae70fbb98a287c5930817
fuminopen <fuminopen@gmail.com>
2022-06-24 13:39:03 +0900

    [add]logic for fail stack

---
+++ app/ValueObjects/FailStack.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\ValueObjects;
+
+use InvalidArgumentException;
+
+final class FailStack
+{
+    /**
+     * @var int
+     */
+    public readonly int $stack;
+
+    /**
+     * __construct
+     *
+     * @param  int|float $stack Accepts float to avoid loss of precision by implicit conversion. Float will be rejected by check on constructor if passed.
+     * @return void
+     */
+    public function __construct(int|float $stack)
+    {
+        if ($this->assertInvalidArguments($stack)) {
+            throw new InvalidArgumentException('Fail stack must be natural integer including zero.');
+        }
+
+        $this->stack = $stack;
+    }
+
+    /**
+     * assertInvalidArguments
+     *
+     * @param  int|float $stack
+     * @return bool
+     */
+    private function assertInvalidArguments(int|float $stack): bool
+    {
+        if (!is_integer($stack)) {
+            return true;
+        }
+
+        if ($stack < 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
