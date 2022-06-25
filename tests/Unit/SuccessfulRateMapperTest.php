0458cd08280edadcc58b8b5d7098e615f00f692b
fuminopen <fuminopen@gmail.com>
2022-06-25 23:29:04 +0900

    [add]test for successful rate increase suddenly dropping after fail stack threshold

--- tests/Unit/SuccessfulRateMapperTest.php
+++ tests/Unit/SuccessfulRateMapperTest.php
@@ -32,7 +32,7 @@ class SuccessfulRateMapperTest extends TestCase
     }
 
     /**
-     * TODO 3
+     * TODO 1
      *
      * @return void
      */
@@ -61,5 +61,31 @@ class SuccessfulRateMapperTest extends TestCase
         $mapperWithFS1 = new SuccessfulRateMapper($equipmentBeyondThreshold, new FailStack(1));
 
         $this->assertTrue($mapperWithNoStack->getRate() < $mapperWithFS1->getRate());
+    }
+
+    public function test_increase_rate_slows_down_after_certain_fail_stack()
+    {
+        // instantiate an equipment with level which successful rate is not 100%
+        $equipmentBeyondThreshold = new Equipment(new EnhancementLevel(8));
+
+        $mapper23 = new SuccessfulRateMapper($equipmentBeyondThreshold, new FailStack(23));
+
+        $mapper24 = new SuccessfulRateMapper($equipmentBeyondThreshold, new FailStack(24));
+
+        $mapper25 = new SuccessfulRateMapper($equipmentBeyondThreshold, new FailStack(25));
+
+        $mapper26 = new SuccessfulRateMapper($equipmentBeyondThreshold, new FailStack(26));
+
+        $diff = $mapper24->getRate() - $mapper23->getRate();
+
+        $this->assertTrue($diff === 2.04);
+
+        $diff = $mapper25->getRate() - $mapper24->getRate();
+
+        $this->assertTrue($diff === 2.04);
+
+        $diff = $mapper26->getRate() - $mapper25->getRate();
+
+        $this->assertTrue($diff === 0.40);
     }
 }
