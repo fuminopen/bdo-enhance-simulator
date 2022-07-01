40deaa2ca2c39514d87596737d2f1dfcaee83092
fuminopen <fuminopen@gmail.com>
2022-07-01 22:37:04 +0900

    [add]test for stack removel

--- tests/Unit/EnhancementDomainTest.php
+++ tests/Unit/EnhancementDomainTest.php
@@ -6,6 +6,11 @@ use App\Domains\EnhancementDomain;
 use App\ValueObjects\Equipment;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * 1. stack can be set
+ * 2. stack can be removed
+ * 3.
+ */
 class EnhancementDomainTest extends TestCase
 {
     public function test_stack_can_be_set()
@@ -17,5 +22,20 @@ class EnhancementDomainTest extends TestCase
         $domain->setFailStack($stack);
 
         $this->assertSame($stack, $domain->currentStack());
+    }
+
+    public function test_stack_can_be_removed()
+    {
+        $domain = new EnhancementDomain(new Equipment());
+
+        $stack = 10;
+
+        $domain->setFailStack($stack);
+
+        $this->assertSame($stack, $domain->currentStack());
+
+        $domain->removeFailStack();
+
+        $this->assertSame(0, $domain->currentStack());
     }
 }
