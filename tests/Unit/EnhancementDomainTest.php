a8bf6f33ac93a490aaf34a8542f3caa323053db5
fuminopen <fuminopen@gmail.com>
2022-07-03 18:02:34 +0900

    [add]test for equipment setting

--- tests/Unit/EnhancementDomainTest.php
+++ tests/Unit/EnhancementDomainTest.php
@@ -3,16 +3,21 @@
 namespace Tests\Unit;
 
 use App\Domains\EnhancementDomain;
+use App\ValueObjects\EnhancementLevel;
 use App\ValueObjects\Equipment;
 use PHPUnit\Framework\TestCase;
 
 /**
  * 1. stack can be set
  * 2. stack can be removed
- * 3.
+ * 3. equipment can be set
+ * 4. equipment can be removed
  */
 class EnhancementDomainTest extends TestCase
 {
+    /**
+     * TODO 1
+     */
     public function test_stack_can_be_set()
     {
         $domain = new EnhancementDomain(new Equipment());
@@ -24,6 +29,9 @@ class EnhancementDomainTest extends TestCase
         $this->assertSame($stack, $domain->currentStack());
     }
 
+    /**
+     * TODO 2
+     */
     public function test_stack_can_be_removed()
     {
         $domain = new EnhancementDomain(new Equipment());
@@ -37,5 +45,19 @@ class EnhancementDomainTest extends TestCase
         $domain->removeFailStack();
 
         $this->assertSame(0, $domain->currentStack());
+    }
+
+    /**
+     * TODO 3
+     */
+    public function test_equipment_can_be_set()
+    {
+        $level = 7;
+
+        $domain = new EnhancementDomain();
+
+        $equipment = new Equipment(new EnhancementLevel($level));
+
+        $this->assertSame($level, $domain->equipmentLevel());
     }
 }
