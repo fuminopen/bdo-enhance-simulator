e08e45c2f25e1dddbd0d9d929e0c1f5542aca7c2
fuminopen <fuminopen@gmail.com>
2024-01-28 20:28:18 +0900

    ValueObjectのEquipmentのコードをModelに移動

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Exceptions\NoLongerEnhanceableException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,5 +18,61 @@ class Equipment extends Model
     public function getSalePrice(): int
     {
         return $this->sale_price;
+    }
+
+    public function getCurrentLevel(): EnhancementLevel
+    {
+        return $this->current_level;
+    }
+
+    public function getThreshold(): EnhancementLevel
+    {
+        return $this->threshold;
+    }
+
+    public function getSuccessfulRatePattern(): SuccessfulRatePattern
+    {
+        return $this->successfulRatePattern;
+    }
+
+    public function enhancementSucceed(): self
+    {
+        if (! $this->enhanceable()) {
+            throw new NoLongerEnhanceableException();
+        }
+
+        return new self(
+            $this->currentLevel->levelUp(),
+            $this->threshold,
+            $this->successfulRatePattern
+        );
+    }
+
+    public function enhancementFailed(): self
+    {
+        if (! $this->enhanceable()) {
+            throw new NoLongerEnhanceableException();
+        }
+
+        if ($this->getCurrentLevel()->getValue() <= $this->getThreshold()->getValue()) {
+            return $this;
+        }
+
+        return new self(
+            $this->getCurrentLevel()->levelDown(),
+            $this->threshold,
+            $this->successfulRatePattern
+        );
+    }
+
+    /**************************************************************
+    *
+    * private methods
+    *
+    **************************************************************/
+
+    private function enhanceable(): bool
+    {
+        return ! $this->getCurrentLevel()->atMaximumLevel();
     }
 }
